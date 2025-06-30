b4c849d022376327a7ceb8a3204fa8fafa04f542
Aftab Alam <aaftabalam18191@gmail.com>
2025-06-30 10:13:06 +0530

    useState hook multiple state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react'
+import React, {  useState } from 'react'
 
 const App = () => {
-  const [color,setColor] = useState('Red');
-  const favColor = ()=>{
-   setColor('Blue')
+
+  //  const [brand,setBrand] = useState('Ferrari');
+  //  const [model,setModel] = useState('Roma');
+  //  const [year,setYear] = useState('2025');
+  //  const [color,setColor] = useState('red');
+  const [car,setCar] = useState({
+    brand:"Ferrari",
+    color:"red",
+    model:"Roma",
+    year:"2025"
+  })
+
+  const changeColor = ()=>{
+    setCar((prev)=>{
+      return {...prev,color:"blue"}
+    })    // we use previous value for updating
   }
+  
+  
   return (
-    <div>
-      <h1>My Favourite color is {color}!</h1>
-      <button onClick={favColor}>Blue</button>
-    </div>
+    <>
+      <h1>My {car.brand}</h1>
+      <h2>It is a {car.color} {car.model} from {car.year}</h2>
+      <button onClick={changeColor}>Blue</button>
+
+      
+    </>
   )
 }
 
